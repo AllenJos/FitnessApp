15ac425454c3058bce0d4cbc27855c1679ffe7fa
AllenJos <44272194+AllenJos@users.noreply.github.com>
2020-08-27 13:56:20 +0530

    Update environment.prod.ts

--- fitness-tracker/src/environments/environment.prod.ts
+++ fitness-tracker/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  firebase: {},
+  firebase: {
+      apiKey: "AIzaSyDCaOVOSxCKgm5rhBOx0OPyx0m_n26WSDk",
+    authDomain: "ng-fitness-tracker-22013.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-22013.firebaseio.com",
+    projectId: "ng-fitness-tracker-22013",
+    storageBucket: "ng-fitness-tracker-22013.appspot.com",
+    messagingSenderId: "768455796667",
+    appId: "1:768455796667:web:fb34bf7c81b3ffecf2c69e"
+  },
 };
